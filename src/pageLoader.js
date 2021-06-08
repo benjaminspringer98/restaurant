bb646fbcdac8fd44ab879b76f520dd11cd52a9b1
Benjamin Springer <benjamin.springer98@gmail.com>
2021-06-08 12:53:19 +0200

    added base site

---
+++ src/pageLoader.js
@@ -0,0 +1,9 @@
+function loadPage() {
+    const content = document.querySelector("#content");
+    const header = document.createElement("header");
+    document.body.append(header);
+    const ul = document.createElement("ul");
+    header.append(ul)
+    
+    
+}
